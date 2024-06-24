9bdd5ef4c905c5486e478b6bf0510f1e3ab4eff2
MariaContu <mariaescontu@gmail.com>
2024-06-23 22:40:01 -0300

    att in servico Assinaturas Validas

--- src/main/java/com/pucrs/microsservicos/ServicoAssinaturasValidas/Dominio/listeners/PagServAssValListener.java
+++ src/main/java/com/pucrs/microsservicos/ServicoAssinaturasValidas/Dominio/listeners/PagServAssValListener.java
@@ -1,14 +1,11 @@
 package com.pucrs.microsservicos.ServicoAssinaturasValidas.Dominio.listeners;
 
-import com.pucrs.microsservicos.ServicoAssinaturasValidas.Dominio.models.AssinaturaValida;
 import com.pucrs.microsservicos.ServicoAssinaturasValidas.Dominio.repositories.IRepAssinaturaValida;
 import com.pucrs.microsservicos.ServicoPagamentos.Dominio.events.PagServAssValEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
 
 @Component
 public class PagServAssValListener {
@@ -19,10 +16,6 @@ public class PagServAssValListener {
     @RabbitListener(queues = "pagamentos.assinaturasvalidas")
     @CacheEvict(value = "assinaturasValidas", key = "#event.codAss")
     public void handlePagServAssValEvent(PagServAssValEvent event) {
-        AssinaturaValida assinaturaValida = new AssinaturaValida();
-        assinaturaValida.setCodAss(event.getCodAss());
-        // Atualiza a data de fim de vigência conforme a lógica de negócio
-        assinaturaValida.setDataFimVigencia(LocalDate.now().plusMonths(1));
-        repAssinaturaValida.save(assinaturaValida);
+        repAssinaturaValida.deleteById(event.getCodAss());
     }
 }
